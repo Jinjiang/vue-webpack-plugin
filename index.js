553caa15a19a33a329f978b7c6186550121e0793
Jinjiang <zhaojinjiang@me.com>
2018-03-26 14:33:25 +0800

    some attempts for webpack plugin

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-function VueWebpackPlugin () {}
+class VueWebpackPlugin {
+  apply (compiler) {
+    compiler.plugin('compilation', compilation => {
+      function getDeps (module) {
+        return module.dependencies.
+          filter(dep => (
+            dep.type === 'harmony side effect evaluation' ||
+            dep.type === 'cjs require')).
+          filter(dep => dep.loc.index !== -1)
+      }
+      function getRequest (dep) {
+        return dep.request
+      }
+      function walk (module) {
+        const deps = getDeps(module)
+        console.group(module.rawRequest)
+        if (deps.length) {
+          deps.map(dep => dep.module).forEach(walk)
+        }
+        console.groupEnd(module.rawRequest)
+      }
 
-VueWebpackPlugin.prototype.apply = compiler => {
-  compiler.plugin('done', compilation => {
-    console.log('This is an plugin!')
-  })
+      // function hook (name) {
+      //   compilation.plugin(name, () => { console.log(name) })
+      // }
+
+      // ['buildModule', 'rebuildModule', 'failedModule', 'succeedModule', 'finishModules', 'finishRebuildingModule', 'seal', 'unseal', 'optimizeDependenciesBasic', 'optimizeDependencies', 'optimizeDependenciesAdvanced', 'afterOptimizeDependencies', 'optimize', 'optimizeModulesBasic', 'optimizeModules', 'optimizeModulesAdvanced', 'afterOptimizeModules', 'optimizeChunksBasic', 'optimizeChunks', 'optimizeChunksAdvanced', 'afterOptimizeChunks', 'optimizeTree', 'afterOptimizeTree', 'optimizeChunkModulesBasic', 'optimizeChunkModules', 'optimizeChunkModulesAdvanced', 'afterOptimizeChunkModules', 'shouldRecord', 'reviveModules', 'optimizeModuleOrder', 'advancedOptimizeModuleOrder', 'beforeModuleIds', 'moduleIds', 'optimizeModuleIds', 'afterOptimizeModuleIds', 'reviveChunks', 'optimizeChunkOrder', 'optimizeChunkIds', 'afterOptimizeChunkIds', 'recordModules', 'recordChunks', 'beforeHash', 'afterHash', 'recordHash', 'record', 'beforeModuleAssets', 'shouldGenerateChunkAssets', 'beforeChunkAssets', 'additionalChunkAssets', 'records', 'additionalAssets', 'optimizeChunkAssets', 'afterOptimizeChunkAssets', 'optimizeAssets', 'afterOptimizeAssets', 'needAdditionalSeal', 'afterSeal', 'chunkHash', 'moduleAsset', 'chunkAsset', 'assetPath', 'needAdditionalPass', 'childCompiler', 'normalModuleLoader'].forEach(hook)
+
+      compilation.plugin('advancedOptimizeModuleOrder', (modules) => {
+        console.log('advancedOptimizeModuleOrder')
+        // adjust module order here
+      })
+    })
+    compiler.plugin('done', compilation => {
+      console.log('done')
+      console.log(compilation.compilation.modules.map(module => module.rawRequest || 'undefined').join('\n'))
+    })
+  }
 }
 
 module.exports = VueWebpackPlugin
